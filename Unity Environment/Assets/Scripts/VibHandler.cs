d672904b8997e839b1870ad3452649892d9cd220
thomasroodnl <zakelijkthomasrood@hotmail.com>
2020-06-25 17:59:55 +0200

    Update documentation, refactor function Reset() to ConfigureVibration()

--- Unity Environment/Assets/Scripts/VibHandler.cs
+++ Unity Environment/Assets/Scripts/VibHandler.cs
@@ -4,6 +4,10 @@ using System;
 using UnityEngine;
 using MLAgents;
 
+// ===============================
+// AUTHOR: Thomas Rood
+// PURPOSE: Class that generates and keeps track of vibration (tactile) events for every touch event of the ball.
+// ===============================
 public class VibHandler : MonoBehaviour
 {
     [Tooltip("Parameters object")]
@@ -21,46 +25,27 @@ public class VibHandler : MonoBehaviour
 
     private System.Random rand = new System.Random();
 
-
     // Start is called before the first frame update
     void Start()
     {
         Academy.Instance.OnEnvironmentReset += () =>
         {
-            Reset();
+            ConfigureVibration();
         };
-        parameterScript = parameterObject.GetComponent<Parameters>();
-        switch (parameterScript.condition)
-        {
-            case Parameters.Condition.Left:
-                vib = this.transform.Find("vib_l").gameObject;
-                break;
-            case Parameters.Condition.Center:
-                vib = this.transform.Find("vib_c").gameObject;
-                break;
-            case Parameters.Condition.Right:
-                vib = this.transform.Find("vib_r").gameObject;
-                break;
-        }
-        switch (parameterScript.stimulation)
-        {
-            case Parameters.Stimulation.synchronous:
-                delayLowerBound = 0.0f;
-                delayUpperBound = 0.1f;
-                break;
-            case Parameters.Stimulation.asynchronous:
-                delayLowerBound = 0.0f;
-                delayUpperBound = 1f;
-                break;
-        }
+        ConfigureVibration();
         lastVib = -60f;
         newVib = -60f;
         vibChanged = false;
     }
 
-    private void Reset()
+    /// <summary>
+    /// Activates the right vibration object according to the parameter setting.
+    /// Configures the vibration (tactile) event delay according to the synchronous/asynchronous parameter setting.
+    /// </summary>
+    private void ConfigureVibration()
     {
         parameterScript = parameterObject.GetComponent<Parameters>();
+
         switch (parameterScript.condition)
         {
             case Parameters.Condition.Left:
@@ -80,19 +65,27 @@ public class VibHandler : MonoBehaviour
                 delayUpperBound = 0.1f;
                 break;
             case Parameters.Stimulation.asynchronous:
-                // TODO update delays
                 delayLowerBound = 0.0f;
                 delayUpperBound = 1f;
                 break;
         }
-}
+    }
 
+    /// <summary>
+    /// Called when the Ball reaches the lowest point of the animation.
+    /// Sets the last (tactile) touch time by adding a random delay to the visual touch time.
+    /// </summary>
+    /// <param name="touchTime"></param>
     public void TouchCallBack(float touchTime)
     {
         newVib = touchTime + delayLowerBound + ((delayUpperBound - delayLowerBound) * (float) rand.NextDouble());
         vibChanged = true;
     }
 
+    /// <summary>
+    /// Get the last vibration (tactile) event time.
+    /// </summary>
+    /// <returns>float lastVib</returns>
     public float GetLastVib()
     {
         if(newVib <= Time.time && vibChanged)
@@ -106,6 +99,7 @@ public class VibHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Show the green vibration indicator in the environment when we reach the lastVib time specified by TouchCallBack(float touchTime).
         if (vib != null) { 
             if (Time.time - lastVib < displayTime && !vib.activeSelf)
             {
