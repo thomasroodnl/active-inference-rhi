1231bd7e5ecc24abc9d14c05882f3e4297864923
thomasroodnl <zakelijkthomasrood@hotmail.com>
2020-06-25 17:45:55 +0200

    Update documentation, remove empty Update() function

--- Unity Environment/Assets/Scripts/Ball.cs
+++ Unity Environment/Assets/Scripts/Ball.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// ===============================
+// AUTHOR: Thomas Rood
+// PURPOSE: Intermediate class that propagates the Ball's animation callback to the BallHandler and VibHandler objects.
+// ===============================
 public class Ball : MonoBehaviour
 {
     [Tooltip("BallHandler object")]
@@ -20,16 +24,14 @@ public class Ball : MonoBehaviour
         vibHandlerScript = vibHandler.GetComponent<VibHandler>();
     }
 
+    /// <summary>
+    /// Function called when the Ball animation is at the lowest point (i.e. touches the hand).
+    /// Given that the animation is two seconds long, this function is called every two seconds.
+    /// </summary>
     public void TouchCallBack()
     {
         float touchTime = Time.time;
         ballHandlerScript.TouchCallBack(touchTime);
         vibHandlerScript.TouchCallBack(touchTime);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
